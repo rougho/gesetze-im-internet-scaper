ddf315b1481c2ec6b43c0f656f373a3e2d745b91
rohi <rohi@rgho.de>
2024-09-10 15:56:42 +0200

    get_page_object paramenter used in fnction

--- scaper.py
+++ scaper.py
@@ -2,17 +2,26 @@ import os
 import json
 from bs4 import BeautifulSoup as bs
 import requests
+from urllib.parse import urljoin
+
 
 BASE_URL = "https://www.gesetze-im-internet.de/"
+
 DIR =  "data/"
+HOME_PAGE_LIST_FNAME = "home_page_list.json"
 
 def write_to_json(data, file_name) -> None:
     os.makedirs(DIR, exist_ok=True)
     with open(os.path.join(DIR, file_name), "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False) 
 
+def load_json_data(file_path = HOME_PAGE_LIST_FNAME)->list:
+    with open(os.path.join(DIR, file_path), "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
+
 def get_page_object(url = BASE_URL) -> object:
-    response = requests.get(BASE_URL)
+    response = requests.get(url)
     response.encoding = 'utf-8' 
     if response.status_code == 200:
         return bs(response.content, "html.parser")
@@ -32,9 +41,22 @@ def home_page_list(obj = get_page_object()) -> list:
                 data.append({"text": text, "href": href})
 
         print(json.dumps(data, indent=4, ensure_ascii=False))
-        write_to_json(data, "home_page_list.json")
+        write_to_json(data, HOME_PAGE_LIST_FNAME)
     else:
         print("Navigation element not found.")
 
 
-home_page_list()
+def get_laws_alphabetically_list(relative_url):
+    full_path = urljoin(BASE_URL, relative_url)
+    page_object = get_page_object(full_path)
+    print(full_path)
+    laws_list = page_object.find(id="content_2022")
+    laws_list = laws_list.find(id="paddingLR12")
+    laws_list = laws_list.find_all('a')
+    return laws_list
+
+
+
+# home_page_list()
+data = load_json_data()
+print(get_laws_alphabetically_list(data[0]['href']))
